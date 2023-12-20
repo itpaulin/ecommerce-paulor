7b314552897cb2bf56d516c828830efe1929fe70
Paulo Ricardo <prmaltaleal@gmail.com>
2023-12-20 00:59:58 -0300

    feat: add host url

--- env.d.ts
+++ env.d.ts
@@ -6,5 +6,7 @@ declare namespace NodeJS {
     NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string;
     STRIPE_SECRET_KEY: string;
     STRIPE_WEBHOOK_SECRET_KEY: string;
+    NEXTAUTH_SECRET: string;
+    HOST_URL: string;
   }
 }
