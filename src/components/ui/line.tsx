714d59470f3876e4182c2e720165f8dbadde6a23
Paulo Ricardo <prmaltaleal@gmail.com>
2023-12-20 12:58:37 -0300

    feat: add component Line

---
+++ src/components/ui/line.tsx
@@ -0,0 +1,20 @@
+const Line = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="3"
+      height="26"
+      viewBox="0 0 3 26"
+      fill="none"
+    >
+      <path
+        d="M1.5 25L1.5 1"
+        stroke="#2A2A2A"
+        stroke-width="2"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+
+export default Line;
