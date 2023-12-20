7d067c560925ee402a61aecf593dce664267e80a
Paulo Ricardo <prmaltaleal@gmail.com>
2023-12-20 01:17:49 -0300

    feat: deals page

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -95,10 +95,17 @@ const Header = () => {
                 </Button>
               </Link>
             </SheetClose>
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <PercentIcon size={16} />
-              Ofertas
-            </Button>
+            <SheetClose asChild>
+              <Link href="/deals">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <PercentIcon size={16} />
+                  Ofertas
+                </Button>
+              </Link>
+            </SheetClose>
 
             <SheetClose asChild>
               <Link href="/catalog">
