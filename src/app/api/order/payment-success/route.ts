7e10353f4c27927bea593a01ca141b8f3f31ef34
Paulo Ricardo <prmaltaleal@gmail.com>
2023-12-20 00:22:03 -0300

    feat: add stripe webhook

---
+++ src/app/api/order/payment-success/route.ts
@@ -0,0 +1,35 @@
+// validar o evento
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2023-10-16",
+});
+
+export const POST = async (request: Request) => {
+  const signature = request.headers.get("stripe-signature");
+
+  if (!signature) {
+    return NextResponse.error();
+  }
+
+  const text = await request.text();
+
+  const event = stripe.webhooks.constructEvent(
+    text,
+    signature,
+    process.env.STRIPE_WEBHOOK_SECRET_KEY,
+  );
+
+  if (event.type === "checkout.session.completed") {
+    const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
+      event.data.object.id,
+      {
+        expand: ["line_items"],
+      },
+    );
+    const lineItems = sessionWithLineItems.line_items;
+  }
+
+  return NextResponse.json({ received: true });
+};
