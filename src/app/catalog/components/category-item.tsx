02593e917bf89d1a7e9c518ece579714eb8ef9a9
Paulo Ricardo <prmaltaleal@gmail.com>
2023-12-21 20:57:20 -0300

    style: catalog page responsive

--- src/app/catalog/components/category-item.tsx
+++ src/app/catalog/components/category-item.tsx
@@ -10,7 +10,7 @@ const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
     <Link href={`/category/${category.slug}`}>
       <div className="flex flex-col">
-        <div className="bg-category-item-gradient flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg ">
+        <div className="bg-category-item-gradient flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg md:h-[185px]">
           <Image
             src={category.imageUrl}
             alt={category.name}
