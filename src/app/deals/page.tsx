25d41de66469306d26feabf7296d25820dd619e3
Paulo Ricardo <prmaltaleal@gmail.com>
2024-01-01 22:18:32 -0300

    style: deals page responsive

--- src/app/deals/page.tsx
+++ src/app/deals/page.tsx
@@ -13,7 +13,7 @@ const DealsPage = async () => {
     },
   });
   return (
-    <div className="flex flex-col gap-8 p-5">
+    <div className="flex flex-col gap-8 p-5 md:px-24">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
@@ -21,7 +21,7 @@ const DealsPage = async () => {
         <PercentIcon size={16} />
         Ofertas
       </Badge>
-      <div className="grid grid-cols-2 gap-8">
+      <div className=" grid grid-cols-2 gap-8 md:grid md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
         {deals.map((product) => (
           <ProductItem
             key={product.id}
