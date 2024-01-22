35860ef33c461efe23f9f17a380b80a943200d1a
Paulo Ricardo <prmaltaleal@gmail.com>
2024-01-21 22:36:32 -0300

    fix: wrong padding to desktop UI

--- src/components/ui/section-title.tsx
+++ src/components/ui/section-title.tsx
@@ -2,7 +2,7 @@ import { ComponentProps } from "react";
 
 const SectionTitle = ({ children, ...props }: ComponentProps<"p">) => {
   return (
-    <p className="mb-3 pl-5 font-bold uppercase" {...props}>
+    <p className="mb-3 pl-5 font-bold uppercase md:pl-0" {...props}>
       {children}
     </p>
   );
