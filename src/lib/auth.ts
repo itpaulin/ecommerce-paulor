43c9b1c5299573f31c4c0cc137faad92aab5a2e7
Paulo Ricardo <prmaltaleal@gmail.com>
2024-09-21 16:03:15 -0300

    chore: ensure use secret to next auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,11 +4,12 @@ import { prismaClient } from "@/lib/prisma";
 import { AuthOptions } from "next-auth";
 
 export const authOptions: AuthOptions = {
-    adapter: PrismaAdapter(prismaClient),
-    providers: [
-        GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }),
-    ],
-}
+  adapter: PrismaAdapter(prismaClient),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
